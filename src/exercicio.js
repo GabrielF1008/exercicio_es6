57205102c8275bd0df615ce88a0bfa0aaa64016b
Gabriel Oehlerick <gabrielfelipe1009@gmail.com>
2024-06-26 23:23:06 -0300

    exercicio

---
+++ src/exercicio.js
@@ -0,0 +1,26 @@
+alunosDoTerceiroAno = [
+    {
+        nome: "Gabriel",
+        nota: 10
+    },
+    {
+        nome: "Felipe",
+        nota: 8
+    },
+    {
+        nome: "Pedro",
+        nota: 4
+    },
+    {
+        nome: "João",
+        nota: 5
+    },
+    {
+        nome: "Gian",
+        nota: 9
+    }
+]
+
+const alunosComNotaMaiorQue6 = alunosDoTerceiroAno.filter((aluno) => {
+    return aluno.nota >= 6
+})
